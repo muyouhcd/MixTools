633f3e3df706b0e349002f34eda380c5446dc108
DYM <45816753+muyouhcd@users.noreply.github.com>
2024-10-21 12:28:42 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "MiAO",
     "author": "MuyouHCD",
-    "version": (4,7,7),
+    "version": (4,7,8),
     "blender": (3, 6, 1),
     "location": "View3D",
     "description": "python.exe -m pip install pillow",
