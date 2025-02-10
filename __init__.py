f7a47d28cb499c1ea6b691b2148f43db2db62e4b
DYM <45816753+muyouhcd@users.noreply.github.com>
2025-02-10 20:52:16 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "MiaoToolBox",
     "author": "MuyouHCD",
-    "version": (4,8,16),
+    "version": (4,8,25),
     "blender": (3, 6, 1),
     "location": "View3D",
     "description": "python.exe -m pip install pillow",
