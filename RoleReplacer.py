d9a8fa47c37f13f4c42384d3647b364cd70007c1
DYM <45816753+muyouhcd@users.noreply.github.com>
2025-05-14 12:03:40 +0800

    修复报错

--- RoleReplacer.py
+++ RoleReplacer.py
@@ -23,7 +23,7 @@ class MIAO_OT_RoleReplacer(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
-        # 获取场景中设置的集合名称
+        # 获取场景中设置的集合
         source_collection = context.scene.collectionA
         target_collection = context.scene.collectionB
 
@@ -31,23 +31,20 @@ class MIAO_OT_RoleReplacer(bpy.types.Operator):
         classified_objects = {key: [] for key in categories}
 
         # 遍历源集合中的物体并分类
-        if source_collection in bpy.data.collections:
-            collection_1 = bpy.data.collections[source_collection]
-            for obj in collection_1.objects:
+        if source_collection:
+            for obj in source_collection.objects:
                 if obj.type == 'MESH':  # 仅针对 Mesh 类型物体
                     for category, keywords in categories.items():
                         if any(keyword in obj.name for keyword in keywords):
                             classified_objects[category].append(obj)  # 存储实际物体对象
                             break
         else:
-            self.report({'ERROR'}, f"源集合 '{source_collection}' 不存在，请检查集合名称是否正确。")
+            self.report({'ERROR'}, f"源集合不存在，请检查集合设置。")
             return {'CANCELLED'}
 
         # 遍历目标集合中的物体
-        if target_collection in bpy.data.collections:
-            collection_2 = bpy.data.collections[target_collection]
-            
-            for obj in collection_2.objects:
+        if target_collection:
+            for obj in target_collection.objects:
                 if obj.type == 'MESH':  # 仅针对 Mesh 类型物体
                     for category, keywords in categories.items():
                         if any(keyword in obj.name for keyword in keywords):
@@ -63,7 +60,7 @@ class MIAO_OT_RoleReplacer(bpy.types.Operator):
                                 self.report({'WARNING'}, f"分类 '{category}' 中没有可替换的物体，跳过物体 '{obj.name}'。")
                             break
         else:
-            self.report({'ERROR'}, f"目标集合 '{target_collection}' 不存在，请检查集合名称是否正确。")
+            self.report({'ERROR'}, f"目标集合不存在，请检查集合设置。")
             return {'CANCELLED'}
 
         return {'FINISHED'}
